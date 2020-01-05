04c07135381635d7039b5d8c4c8da53619da2aa0
Rabee4221 <vladis.4221@gmail.com>
2020-01-05 05:46:52 +0200

    add homework 33

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.js-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+});
+
+
